7af9415c76ad9ec96273ad73166bc98ca9f53252
housirvip <housirvip@outlook.com>
2019-02-13 20:23:20 +0800

    增加 domain 和 rule 准备动态适配规则

---
+++ src/main/java/vip/housir/bookspider/entity/Domain.java
@@ -0,0 +1,22 @@
+package vip.housir.bookspider.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author housirvip
+ */
+@Data
+public class Domain implements Serializable {
+
+    private Integer id;
+
+    private String domain;
+
+    private Integer bookId;
+
+    private Integer chapterId;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/vip/housir/bookspider/entity/Rule.java
@@ -0,0 +1,26 @@
+package vip.housir.bookspider.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author housirvip
+ */
+@Data
+public class Rule implements Serializable {
+    private Integer id;
+
+    private Integer domainId;
+
+    private String type;
+
+    private String key;
+
+    private String xpath;
+
+    private List<String> regexp;
+
+    private static final long serialVersionUID = 1L;
+}
